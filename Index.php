f76a78236960ccd30bc84a1cee2d6ebca77ca3b8
Rah224 <119221267+Rah224@users.noreply.github.com>
2022-11-27 17:46:27 +0100

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,10 @@
+<form action="db_connect.php" method="POST">
+<input type="text" name="email" placeholder="Email Address" id='email' required/>
+<input type="Password" name="password" placeholder="Password" id="password" required/>
+<div class="submit">
+<input type="submit" value="Login" >
+</div>
+<span class="forget-pass">
+<a href="#">Forgot Password?</a>
+</span>
+</form>
